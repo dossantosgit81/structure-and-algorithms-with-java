e60d252b6d7a491012a5853424384acbd356fb5a
Rafael Mendes <dossantosgit81@gmail.com>
2025-12-10 11:05:06 -0300

    create sketch linked list

---
+++ src/main/java/org/example/linkedlist/LinkedList.java
@@ -0,0 +1,18 @@
+package org.example.linkedlist;
+
+import org.example.linkedlist.model.Node;
+
+public class LinkedList {
+
+    private Node head;
+    private int count;
+
+    public LinkedList(){
+        this.head = null;
+        this.count = 0;
+    }
+
+    public void push(Node element){
+    }
+
+}
---
+++ src/main/java/org/example/linkedlist/model/Node.java
@@ -0,0 +1,7 @@
+package org.example.linkedlist.model;
+
+public class Node {
+
+    private int element;
+    private Node next;
+}
