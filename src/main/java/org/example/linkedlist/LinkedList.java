0f214a164f5452e0ece36555f15d799d75c9ef45
Rafael Mendes <dossantosgit81@gmail.com>
2025-12-12 11:17:17 -0300

    create feature push and print

--- src/main/java/org/example/linkedlist/LinkedList.java
+++ src/main/java/org/example/linkedlist/LinkedList.java
@@ -12,7 +12,26 @@ public class LinkedList {
         this.count = 0;
     }
 
-    public void push(Node element){
+    public void push(int element){
+        Node node = new Node(element);
+        if (head == null) {
+            head = node;
+        }else {
+            Node current = head;
+            while (current.getNext() != null){
+                current = current.getNext();
+            }
+            current.setNext(node);
+        }
+        this.count++;
+    }
+
+    public void print(){
+        Node current = head;
+        while (current != null){
+            System.out.println(current.getElement());
+            current = current.getNext();
+        }
     }
 
 }
--- src/main/java/org/example/linkedlist/model/Node.java
+++ src/main/java/org/example/linkedlist/model/Node.java
@@ -4,4 +4,20 @@ public class Node {
 
     private int element;
     private Node next;
+
+    public Node(int element){
+        this.element = element;
+    }
+
+    public int getElement() {
+        return element;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
