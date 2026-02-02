d7d98d385603c4bd61d870fd2f798cf28e7fc9eb
Rafael Mendes <dossantosgit81@gmail.com>
2026-02-02 15:03:49 -0300

    Buscando o entendimento de recuursão

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,11 @@ public class Main {
 
         Recursion recursion = new Recursion();
 
-        // FB Iterative
-        int result = recursion.fibIterative(8);
+        // TODO Fibonacci recursive
+        int result = recursion.fibRecursion(5);
         System.out.println(result);
 
         // TODO fatorial iterativo
-
-        // TODO Fibonacci iterative
 
         //TODO Fatorial recursivo
 
--- src/main/java/org/example/reucursion/Recursion.java
+++ src/main/java/org/example/reucursion/Recursion.java
@@ -17,4 +17,27 @@ public class Recursion {
         }
         return fibN;
     }
+
+    /*
+    * Pelo o que entendi, para cada rodada eu vou precisar x + y.
+    * Só que a execução do computador não é linear como em loops.
+    * então ele fará o cálculo de toda linha de execução quando n = 5 para x
+    * depois n = 4 para x
+    * depois n = 3 para x e n = 3 y
+    * então, ele devolve o resuultado para linha de execução de x, que é a soma de x + y para 3, que seria 2 para 5
+    * então ele volta com 2 para x e cálcula n = 4 para y
+    * a soma retorna 3 quando x = 4
+    * então retorna 2 para x quando n = 5
+    * então retorna o resultado final da soma entre x + y
+    * neste caso x = 3 e y = 2
+    * Resultando em 5
+    * */
+    public int fibRecursion(int n){
+        if(n < 1) return 0;
+        if(n <= 2) return 1;
+        int x = fibRecursion(n-1);
+
+        int y = fibRecursion(n-2);
+        return x + y;
+    }
 }
